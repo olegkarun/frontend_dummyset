e1844fee9c61930b0959f3b0f674ece29e87d600
oleg.karun <oleg.karun@ahoii.net>
2019-01-25 21:04:43 +0200

    arrow animation

--- public/assets/Javascript/src/app.js
+++ public/assets/Javascript/src/app.js
@@ -9,15 +9,15 @@ const tbUI = (function ($) {
 
         $('.js-tilt').tilt({
             maxTilt: 30,
-            perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
+            perspective: 800, // Transform perspective, the lower the more extreme the tilt gets.
             easing: "cubic-bezier(0.905, 0.160, 0.100, 0.900)", // Easing on enter/exit.
             scale: 1, // 2 = 200%, 1.5 = 150%, etc..
-            speed: 100, // Speed of the enter/exit transition.
-            transition: true, // Set a transition on enter/exit.
+            speed: 250, // Speed of the enter/exit transition.
+            transition: false, // Set a transition on enter/exit.
             disableAxis: null, // What axis should be disabled. Can be X or Y.
             reset: true, // If the tilt effect has to be reset on exit.
             glare: false, // Enables glare effect
-            maxGlare: 1       // From 0 - 1. 
+            maxGlare: 0     // From 0 - 1. 
         });
 
         var $compass = $('.compass'),
@@ -25,8 +25,11 @@ const tbUI = (function ($) {
 
         $controls.hover(function () {
             var target = $(this).data('blur'),
-                    $blurs = $('.blurs .blur');
+                    $blurs = $('.blurs .blur'),
+                    $links = $('.controls .link');
             $blurs.removeClass('hover');
+            $links.removeClass('hover');
+            $(this).addClass('hover');
             $('.blurs').find(target).addClass('hover');
         });
 
