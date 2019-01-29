5157a16defead5d2ccf3459e72096e501011b7a3
oleg.karun <oleg.karun@ahoii.net>
2019-01-29 17:37:03 +0200

    Basic TYPO3 front build

--- public/assets/Javascript/src/app.js
+++ public/assets/Javascript/src/app.js
@@ -8,14 +8,15 @@ const tbUI = (function ($) {
         console.log(testVar);
 
         $('.js-tilt').tilt({
-            maxTilt: 30,
+            maxTilt: 35,
             perspective: 800, // Transform perspective, the lower the more extreme the tilt gets.
-            easing: "cubic-bezier(0.905, 0.160, 0.100, 0.900)", // Easing on enter/exit.
+            easing: "ease", // Easing on enter/exit.
             scale: 1, // 2 = 200%, 1.5 = 150%, etc..
-            speed: 250, // Speed of the enter/exit transition.
+            speed: 200, // Speed of the enter/exit transition.
             transition: false, // Set a transition on enter/exit.
             disableAxis: null, // What axis should be disabled. Can be X or Y.
-            reset: true, // If the tilt effect has to be reset on exit.
+            reset: false, // allow dont jump when hover lost
+            
             glare: false, // Enables glare effect
             maxGlare: 0     // From 0 - 1. 
         });
@@ -41,6 +42,7 @@ const tbUI = (function ($) {
         $compass.mouseleave(function () {
             $('.blurs .blur').removeClass('hover');
             $('.interactiv-nav .nav-item .overlay').removeClass('hover');
+            $('.controls .link').removeClass('hover');
         });
 
 
